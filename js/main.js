e4f4bdfb05b42825c8c90ec9a5785ac2946c8204
Vera Mstislavtseva <mstislavtsevavv@gmail.com>
2022-12-20 21:28:39 +0300

    слайдер + секция result

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ $(function () {
     loop: true,
     margin: 30,
     nav: true,
+    navText: ['<img src="img/arrow-left.svg">', '<img src="img/arrow-right.svg">'],
+    dots: false,
+    // autoplay: true,
+    startPosition: 1,
     responsive: {
       0: {
         items: 1
@@ -11,7 +15,7 @@ $(function () {
         items: 1
       },
       1000: {
-        items: 2
+        items: 1
       }
     }
   });
