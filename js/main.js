930fb499f4d1a65773ca5a423519553bb108db01
Vera Mstislavtseva <mstislavtsevavv@gmail.com>
2022-12-21 20:26:59 +0300

    секция works + табы jquery

--- js/main.js
+++ js/main.js
@@ -19,4 +19,12 @@ $(function () {
       }
     }
   });
+
+  $('ul.tabs__caption').on('click', 'li:not(.active)', function (e) {
+      e.preventDefault();
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+
 });
